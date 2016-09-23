88972313650a4dbf7040bbe48539ca82b4860475
Morgan Roff <morgan@connectiv8.com>
2016-09-23 15:22:51 -0400

    Swapped from a BackgroundTask to an App

---
+++ WizardsChess/WizardsChessApp/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WizardsChessApp.VoiceControl;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WizardsChessApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+		}
+
+		private async void Button_Click(object sender, RoutedEventArgs e)
+		{
+			System.Diagnostics.Debug.WriteLine("Starting task\n");
+
+			var cmdRecognizer = await CommandRecognizer.CreateAsync();
+			var result = await cmdRecognizer.RecognizeSpeechAsync();
+
+			System.Diagnostics.Debug.WriteLine(result.Text + "\nConfidence: " + result.Confidence);
+		}
+	}
+}
