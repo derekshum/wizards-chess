ba4459b1b887e6199377da7634fc8333ec48d6b9
MorganRoff <morgan@connectiv8.com>
2016-09-28 01:09:04 -0400

    Update voice-control to use SRGS files

---
+++ WizardsChess/WizardsChessApp/VoiceControl/CommandConstraints.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.SpeechRecognition;
+using Windows.Storage;
+
+namespace WizardsChessApp.VoiceControl
+{
+	enum GrammarConstraints
+	{
+		MoveCommands,
+		YesNoCommands
+	}
+
+	static class CommandConstraints
+	{
+		public static async Task<IList<ISpeechRecognitionConstraint>> GetConstraintsAsync()
+		{
+			var grammarList = new List<ISpeechRecognitionConstraint>();
+
+			var commandsFileTask = StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///VoiceControl/MoveCommands.grxml")).AsTask();
+			var yesNoFileTask = StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///VoiceControl/YesNoCommands.grxml")).AsTask();
+			await Task.WhenAll(commandsFileTask, yesNoFileTask);
+
+			moveGrammar = new SpeechRecognitionGrammarFileConstraint(commandsFileTask.Result, k_moveCommands);
+			yesNoGrammar = new SpeechRecognitionGrammarFileConstraint(yesNoFileTask.Result, k_yesNoCommands);
+
+			grammarList.Add(moveGrammar);
+			grammarList.Add(yesNoGrammar);
+
+			return grammarList;
+		}
+
+		public static void EnableGrammar(GrammarConstraints grammar)
+		{
+			switch (grammar)
+			{
+				case GrammarConstraints.MoveCommands:
+					moveGrammar.IsEnabled = true;
+					break;
+				case GrammarConstraints.YesNoCommands:
+					yesNoGrammar.IsEnabled = true;
+					break;
+				default:
+					break;
+			}
+		}
+
+		public static void DisableGrammar(GrammarConstraints grammar)
+		{
+			switch (grammar)
+			{
+				case GrammarConstraints.MoveCommands:
+					moveGrammar.IsEnabled = false;
+					break;
+				case GrammarConstraints.YesNoCommands:
+					yesNoGrammar.IsEnabled = false;
+					break;
+				default:
+					break;
+			}
+		}
+
+		private static ISpeechRecognitionConstraint moveGrammar;
+		private static ISpeechRecognitionConstraint yesNoGrammar;
+
+		private static readonly string k_moveCommands = "moveCommands";
+		private static readonly string k_yesNoCommands = "yesNoCommands";
+	}
+}
---
+++ WizardsChess/WizardsChessApp/VoiceControl/CommandRecognizer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.SpeechRecognition;
+
+namespace WizardsChessApp.VoiceControl
+{
+	class CommandRecognizer
+	{
+		private CommandRecognizer()
+		{
+			SpeechRecognizer_ = new SpeechRecognizer();
+		}
+
+		public static async Task<CommandRecognizer> CreateAsync()
+		{
+			var recognizer = new CommandRecognizer();
+			var compilationResult = await recognizer.SetupConstraintsAsync();
+			if (compilationResult.Status != SpeechRecognitionResultStatus.Success)
+			{
+				throw new FormatException($"Could not compile grammar constraints. Received error {compilationResult.Status}");
+			}
+			return recognizer;
+		}
+
+		public async Task<SpeechRecognitionResult> RecognizeSpeechAsync()
+		{
+			return await SpeechRecognizer_.RecognizeWithUIAsync().AsTask();
+		}
+
+		protected async Task<SpeechRecognitionCompilationResult> SetupConstraintsAsync()
+		{
+			var grammarConstraints = await CommandConstraints.GetConstraintsAsync();
+			foreach (var constraint in grammarConstraints)
+			{
+				SpeechRecognizer_.Constraints.Add(constraint);
+			}
+			return await SpeechRecognizer_.CompileConstraintsAsync().AsTask();
+		}
+
+		private SpeechRecognizer SpeechRecognizer_;
+		private Task<SpeechRecognitionCompilationResult> ConstraintsTask_;
+	}
+}
