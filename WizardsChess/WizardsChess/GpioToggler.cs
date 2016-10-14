2d0377fdc2b0a0f2f57153c3af28d15034c4dd9e
MorganRoff <morgan@connectiv8.com>
2016-10-14 11:41:04 -0400

    Add BackgroundTask for Gpio Toggling

---
+++ WizardsChess/WizardsChess/GpioToggler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace WizardsChess
+{
+	public sealed class GpioToggler
+	{
+		public GpioToggler(TimeSpan period, int pinNum)
+		{
+			this.period = period;
+			gpio = GpioController.GetDefault();
+			pin = gpio.OpenPin(pinNum);
+		}
+
+		public void Start()
+		{
+			pin.Write(GpioPinValue.High);
+			currentPinVal = GpioPinValue.High;
+			pin.SetDriveMode(GpioPinDriveMode.Output);
+
+			toggleTask = Task.Run(async () => {
+				while (true)
+				{
+					await Task.Delay(period);
+					toggle();
+				}
+			});
+		}
+		
+		private void toggle()
+		{
+			switch (currentPinVal)
+			{
+				case GpioPinValue.High:
+					pin.Write(GpioPinValue.Low);
+					currentPinVal = GpioPinValue.Low;
+					break;
+				case GpioPinValue.Low:
+				default:
+					pin.Write(GpioPinValue.High);
+					currentPinVal = GpioPinValue.High;
+					break;
+			}
+		}
+
+		private TimeSpan period;
+		private GpioPinValue currentPinVal;
+		private GpioPin pin;
+		private GpioController gpio;
+		private Task toggleTask;
+	}
+}
