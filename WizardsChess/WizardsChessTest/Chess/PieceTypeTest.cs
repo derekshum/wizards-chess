26b90029d40f02b649b903dbb1c05ccb2cb42535
Morgan Roff <morgan@connectiv8.com>
2016-11-02 21:02:43 -0400

    Add unit test project and Class Library project

---
+++ WizardsChess/WizardsChessTest/Chess/PieceTypeTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace WizardsChessTest
+{
+	[TestClass]
+	public class PieceTypeTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
