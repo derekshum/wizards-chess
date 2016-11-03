00ea16c465d0beb661d12908edd028274753791f
derekshum <12ds27@queensu.ca>
2016-11-02 21:01:06 -0400

    MovementPlanner updated based on Morgan's feedback.

--- WizardsChess/WizardsChessApp/Movement/MovementPlanner.cs
+++ WizardsChess/WizardsChessApp/Movement/MovementPlanner.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WizardsChessApp.Movement; //right?
 using WizardsChessApp.Chess;
 
 namespace WizardsChessApp.Movement
@@ -43,21 +42,18 @@ namespace WizardsChessApp.Movement
 
         private List<Point2D> getMovePath(Point2D start, Point2D end)
         {
-            List<Point2D> path = new List<Point2D>();
-            
             Point2D startPoint = pointConversion(start);
             Point2D endPoint = pointConversion(end);
 
             //could check for null here, if unsure if that check isn't done elsewhere
             if (board.getPieceAt(start).CanJump)
             {
-                path = getKnightMovePath(startPoint, endPoint);
+                return getKnightMovePath(startPoint, endPoint);
             }
             else // piece moving is a non knight piece
             {
-                path = getDirectMovePath(startPoint, endPoint);
+                return getDirectMovePath(startPoint, endPoint);
             }
-            return path;
         }
 
         //gets the direct path of movement for non-knight piece moves (but not that of taken pieces)
@@ -81,22 +77,22 @@ namespace WizardsChessApp.Movement
             int yMove = endPoint.Y - startPoint.Y;
             int xVsY = Math.Abs(xMove) - Math.Abs(yMove);   //should be -2 or 2
 
-            path.Add(new Point2D(startPoint.X, startPoint.Y));
+            path.Add(startPoint);
             if (xVsY == 2)  //moving further in the x direction
             {
-                path.Add(new Point2D(path[path.Count - 1].X, path[path.Count - 1].Y + 1));
+                path.Add(new Point2D(path[path.Count - 1].X, path[path.Count - 1].Y + yMove/2));
                 path.Add(new Point2D(endPoint.X, path[path.Count - 1].Y));
             }
             else if(xVsY == -2 )    //moving further in the y direction
             {
-                path.Add(new Point2D(path[path.Count - 1].X + 1, path[path.Count - 1].Y));
+                path.Add(new Point2D(path[path.Count - 1].X + xMove/2, path[path.Count - 1].Y));
                 path.Add(new Point2D(path[path.Count - 1].X, endPoint.Y));
             }
             else
             {
                 //! error
             }
-            path.Add(new Point2D(endPoint.X, endPoint.Y));
+            path.Add(endPoint);
 
             return path;
         }
@@ -115,15 +111,10 @@ namespace WizardsChessApp.Movement
             {
                 path = getTakenPathWithTeam(startPoint, whiteRemovalDir, whiteEmptyCol, whiteTakenCol, whiteTakenStart - whiteTakenAddDir * numTaken);
             }
-            else if(team == ChessTeam.Black)
+            else    //team == ChessTeam.Black
             {
                 path = getTakenPathWithTeam(startPoint, blackRemovalDir, blackEmptyCol, blackTakenCol, whiteTakenStart - whiteTakenAddDir * numTaken);
             }
-            else
-            {
-                 //! error
-            }
-
             return path;
         }
 
@@ -133,41 +124,42 @@ namespace WizardsChessApp.Movement
             //the number of half squares from the edge of the board to put the first taken piece at
             List<Point2D> path = new List<Point2D>();
 
-            path.Add(new Point2D(startPoint.X, startPoint.Y));
-            path.Add(new Point2D(path[path.Count - 1].X, path[path.Count - 1].Y + dir));    //only needs to move half a square, so no multiplier needed on Dir
+            path.Add(startPoint);
+            path.Add(new Point2D(startPoint.X, startPoint.Y + dir));    //only needs to move half a square, so no multiplier needed on Dir
             if (path[path.Count - 1].Y != troughIndex) //only need to reposition in the empty column if it cannot go straight across
             {
                 path.Add(new Point2D(emptyCol, path[path.Count - 1].Y));
                 path.Add(new Point2D(path[path.Count - 1].X, troughIndex));
             }
-            path.Add(new Point2D(takenCol, path[path.Count - 1].Y));
+            path.Add(new Point2D(takenCol, troughIndex));
             return path;
         }
 
         //converts from an (0-7,0-7) point to a (0-22, 0-16) point
         private Point2D pointConversion(Point2D point)
         {
-            int spacing = 2;    //the number of points per square in one dimension
-            int xOffset = 5;    //the x index of the centre of the left most playing board column
-            int yOffset = 1;    //the y index of the centre of the bottom row
             return new Point2D(xOffset + spacing * point.X, yOffset + spacing + point.Y);
         }
 
         private ChessBoard board;
-        
+
+        //constants pointConversion needs
+        const int spacing = 2;    //the number of points per square in one dimension
+        const int xOffset = 5;    //the x index of the centre of the left most playing board column
+        const int yOffset = 1;    //the y index of the centre of the bottom row
         //constants getTakenPath needs
         const int whiteEmptyCol = 20;     //index of the centre of the empty column in between the board and white taken pieces
         const int blackEmptyCol = 2;      //index of the centre of the empty column in between the board and black taken pieces
-        const int whiteTakenCol = 22;     //index of the centre of the column where white taken pieces are stored
-        const int blackTakenCol = 0;      //index of the centre of the column where black taken pieces are stored
-        const int whiteRemovalDir = -1;   //direction the white pieces are pulled a half square in before being taken off the board
-        const int blackRemovalDir = 1;    //direction the black pieces are pulled a half square in before being taken off the board
-        const int whiteTakenStart = 1;    //index where the first white piece taken is placed
-        const int blackTakenStart = 15;   //index where the first black piece taken is placed
-        const int whiteTakenAddDir = 1;   //direction white pieces are added to the white taken trough
-        const int blackTakenAddDir = -1;  //direction black pieces are added to the black taken trough
+        const int whiteTakenCol = 22;     //x-index of the centre of the column where white taken pieces are stored
+        const int blackTakenCol = 0;      //x-index of the centre of the column where black taken pieces are stored
+        const int whiteTakenStart = 1;    //y-index where the first white piece taken is placed
+        const int blackTakenStart = 15;   //y-index where the first black piece taken is placed
+        const int whiteRemovalDir = -1;   //y-direction the white pieces are pulled a half square in before being taken off the board
+        const int blackRemovalDir = 1;    //y-direction the black pieces are pulled a half square in before being taken off the board
+        const int whiteTakenAddDir = 1;   //y-direction white pieces are added to the white taken trough
+        const int blackTakenAddDir = -1;  //y-direction black pieces are added to the black taken trough
 
-        //! debugging printer
+        //TODO: axe this debugging printer when it's no longer needed?
         public void Print(List<List<Point2D>> paths)
         {
             paths.ForEach(delegate (List<Point2D> path) {
