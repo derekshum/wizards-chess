826c65cbb8cea3a6f56dd5ce66303139744b1293
derekshum <12ds27@queensu.ca>
2016-11-12 20:48:11 -0500

    Created ChessLogic, need to understand pieces taken by to go further

---
+++ WizardsChess/WizardsChessUtils/Chess/ChessLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WizardsChess.Chess.Pieces;
+using WizardsChess.Movement;
+
+namespace WizardsChess.Chess
+{
+	class ChessLogic
+	{
+		public ChessLogic (ChessBoard b)
+		{
+
+		}
+
+
+	}
+}
