a9fad8ff398f13d7825671cbf1c486b287fa5717
Morgan Roff <morgan@connectiv8.com>
2016-11-30 15:23:04 -0500

    Attempted fix to background app Communicator

--- WizardsChess/WizardsChess/VoiceControl/Communicator.cs
+++ WizardsChess/WizardsChess/VoiceControl/Communicator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Media.Playback;
 using Windows.Media.SpeechSynthesis;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -14,43 +15,44 @@ namespace WizardsChess.VoiceControl
 		public Communicator()
 		{
 			speechSynth = new SpeechSynthesizer();
-			//try
-			//{
-			//	audioOut = new MediaElement();
-			//}
-			//catch (Exception e)
-			//{
-			//	System.Diagnostics.Debug.WriteLine(e);
-			//}
-			//audioOut.MediaEnded += audioEventReceived;
-			//audioOut.MediaFailed += audioEventReceived;
-			//audioOut.MediaOpened += audioEventReceived;
+			try
+			{
+				audioOut = Windows.Media.Playback.BackgroundMediaPlayer.Current;
+			}
+			catch (Exception e)
+			{
+				System.Diagnostics.Debug.WriteLine(e);
+			}
+			audioOut.MediaEnded += audioEventReceived;
+			audioOut.MediaFailed += audioEventReceived;
+			audioOut.MediaOpened += audioEventReceived;
 		}
 
 		public async Task Speak(string text)
 		{
 			await Threading.MarshallToUiThread(async () =>
 			{
-				if (audioOut.CurrentState == Windows.UI.Xaml.Media.MediaElementState.Playing)
+				if (audioOut.PlaybackSession.PlaybackState == MediaPlaybackState.Playing)
 				{
-					audioOut.Stop();
+					audioOut.Pause();
 				}
 				var voiceStream = await speechSynth.SynthesizeTextToStreamAsync(text);
 				if (voiceStream == null)
 				{
 					System.Diagnostics.Debug.WriteLine($"Could not synthesize voice stream from text: {text}.");
 				}
-				audioOut.SetSource(voiceStream, voiceStream.ContentType);
+				
+				audioOut.Source = Windows.Media.Core.MediaSource.CreateFromStream(voiceStream, voiceStream.ContentType);
 				audioOut.Play();
 			});
 		}
 
-		private void audioEventReceived(object sender, RoutedEventArgs e)
+		private void audioEventReceived(MediaPlayer sender, object e)
 		{
 			System.Diagnostics.Debug.WriteLine($"Received audio event: {e.ToString()}");
 		}
 
 		private SpeechSynthesizer speechSynth;
-		private MediaElement audioOut;
+		private MediaPlayer audioOut;
 	}
 }
