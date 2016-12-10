62a8e92217480518b5dbfda50e9711be3f891d6f
MorganR <roffmorgan@gmail.com>
2016-12-10 02:36:46 -0500

    Fixed MoveTesting test from previous commit

--- WizardsChess/WizardsChessTest/Movement/MoveTesting.cs
+++ WizardsChess/WizardsChessTest/Movement/MoveTesting.cs
@@ -23,43 +23,52 @@ namespace WizardsChessTest.Movement
 		{
 			ChessLogic logic = new ChessLogic();
 			movePlanner = new MovePlanner(logic.Board);
-			xGridMover = constructMover();
-			yGridMover = constructMover();
+			constructAnAxis(ref xMover, ref xCalibrator, ref xPreciseMover, ref xGridMover);
+			constructAnAxis(ref yMover, ref yCalibrator, ref yPreciseMover, ref yGridMover);
 			var magnet = new MockMagnet();
 			movePerformer = new MovePerformer(xGridMover, yGridMover, magnet);
 			moveManager = new MoveManager(movePlanner, movePerformer);
 		}
 
 		[TestMethod]
-		public void TestMoveManagerMove()
+		public void TestMovePerformerMove()
 		{
 			TestMoveComponentCompilation();
 
-			moveManager.MoveAsync(new Point2D(-1, -1), new Point2D(2, 1));
-			Assert.AreEqual(xGridMover.GridPosition, 0, "The xMotor didn't return to position zero after a move.");
-			Assert.AreEqual(yGridMover.GridPosition, 0, "The yMotor didn't return to position zero after a move.");
+			int targetX = 2;
+			int targetY = -2;
+			movePerformer.CalibrateAsync().Wait();
+			var list = new List<Point2D>() { new Point2D(-1, -1), new Point2D(targetX, targetY) };
+			movePerformer.MovePieceAsync(list).Wait();
+			Assert.AreEqual(targetX, xGridMover.GridPosition,"The xMotor didn't end where expected.");
+			Assert.AreEqual(targetY, yGridMover.GridPosition, "The yMotor didn't end where expected.");
+			Assert.AreEqual(targetX, (int)Math.Round((float)xPreciseMover.Position / xPreciseMover.StepsPerGridUnit), "xPreciseMover was not where expected.");
+			Assert.AreEqual(targetY, (int)Math.Round((float)yPreciseMover.Position / yPreciseMover.StepsPerGridUnit), "yPreciseMover was not where expected.");
 		}
 
-		private IPreciseMotorMover xMover;
-		private IPreciseMotorMover yMover;
+		private IMotorMover xMover;
+		private IMotorMover yMover;
+		private IMotorCalibrator xCalibrator;
+		private IMotorCalibrator yCalibrator;
+		private IPreciseMotorMover xPreciseMover;
+		private IPreciseMotorMover yPreciseMover;
 		private IGridMotorMover xGridMover;
 		private IGridMotorMover yGridMover;
 		private IMovePerformer movePerformer;
 		private IMovePlanner movePlanner;
 		private IMoveManager moveManager;
 
-		private IGridMotorMover constructMover()
+		private void constructAnAxis(ref IMotorMover mover, ref IMotorCalibrator calibrator, ref IPreciseMotorMover preciseMover, ref IGridMotorMover gridMover)
 		{
 			var motor = MockMotor.Create();
 			var locator = new MotorLocator(new MockGpio(), motor.Information);
 			var signaler = new PositionSignaler(locator);
-			var basicMover = new MotorMover(3, signaler, locator, motor);
+			mover = new MotorMover(3, signaler, locator, motor);
 			var topInterrupter = new MockPhotoInterrupter(1, 4, 6, locator, motor);
 			var bottomInterrupter = new MockPhotoInterrupter(-1, -6, -4, locator, motor);
-			var calibrator = new MotorCalibrator(-5, 5, basicMover, motor.Information, topInterrupter, bottomInterrupter);
-			var preciseMover = new PreciseMotorMover(basicMover, calibrator);
-			var gridMover = new GridMotorMover(preciseMover);
-			return gridMover;
+			calibrator = new MotorCalibrator(-5, 5, mover, motor.Information, topInterrupter, bottomInterrupter);
+			preciseMover = new PreciseMotorMover(mover, calibrator);
+			gridMover = new GridMotorMover(preciseMover);
 		}
 	}
 }
